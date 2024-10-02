2cf4ea6d221697a5b27f366e1843c8fe9e417092
patamarcelo <patamarcelo@gmail.com>
2024-10-02 17:14:23 -0300

    changed version for deploy

---
+++ screens/plot-helper.js
@@ -0,0 +1,21 @@
+export const newMapArr = (mapPlot) => {
+    console.log(mapPlot)
+    const newArrMap = mapPlot.map((data) => {
+        const newArr = data.map_geo_points.map((lonLat) => {
+            return {
+                latitude: parseFloat(lonLat.latitude),
+                longitude: parseFloat(lonLat.longitude)
+            };
+        });
+        return {
+            talhaoCenterGeo: data.map_centro_id,
+            talhao: data.talhao__id_talhao,
+            farmCenterGeo: data.talhao__fazenda__map_centro_id,
+            farmName: data.talhao__fazenda__nome,
+            coords: newArr
+        };
+    });
+
+    return newArrMap
+
+} 
