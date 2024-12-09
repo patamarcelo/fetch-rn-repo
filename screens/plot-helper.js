1e3723621d06bc33f86b43a487e33e376d70dc16
patamarcelo <patamarcelo@gmail.com>
2024-12-09 09:22:38 -0300

    adjust bug error

--- screens/plot-helper.js
+++ screens/plot-helper.js
@@ -1,17 +1,17 @@
 export const newMapArr = (mapPlot) => {
     console.log(mapPlot)
-    const newArrMap = mapPlot.map((data) => {
-        const newArr = data.map_geo_points.map((lonLat) => {
+    const newArrMap = mapPlot?.map((data) => {
+        const newArr = data?.map_geo_points?.map((lonLat) => {
             return {
-                latitude: parseFloat(lonLat.latitude),
-                longitude: parseFloat(lonLat.longitude)
+                latitude: parseFloat(lonLat?.latitude),
+                longitude: parseFloat(lonLat?.longitude)
             };
         });
         return {
-            talhaoCenterGeo: data.map_centro_id,
-            talhao: data.talhao__id_talhao,
-            farmCenterGeo: data.talhao__fazenda__map_centro_id,
-            farmName: data.talhao__fazenda__nome,
+            talhaoCenterGeo: data?.map_centro_id,
+            talhao: data?.talhao__id_talhao,
+            farmCenterGeo: data?.talhao__fazenda__map_centro_id,
+            farmName: data?.talhao__fazenda__nome,
             coords: newArr
         };
     });
