6b3d21f9f8e7c2fb4db33be6a8ed6395849c0cf3
patamarcelo <patamarcelo@gmail.com>
2025-07-04 22:06:00 -0300

    updated again

--- android/app/src/main/java/com/patamarcelo/fetchapp/MainActivity.kt
+++ android/app/src/main/java/com/patamarcelo/fetchapp/MainActivity.kt
@@ -8,28 +8,15 @@ import com.facebook.react.defaults.DefaultReactActivityDelegate
 
 class MainActivity : ReactActivity() {
 
-  /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
-   * rendering of the component.
-   */
   override fun getMainComponentName(): String = "main"
 
-  /**
-   * Returns the instance of the [ReactActivityDelegate]. We use a util class [DefaultReactActivityDelegate]
-   * which allows you to easily enable Fabric and Concurrent React (aka React 18) with two boolean flags.
-   */
   override fun createReactActivityDelegate(): ReactActivityDelegate {
     return DefaultReactActivityDelegate(
         this,
         mainComponentName,
-        // If you opted-in for the New Architecture, we enable the Fabric Renderer.
         DefaultNewArchitectureEntryPoint.getFabricEnabled())
   }
 
-  /**
-   * Adicione este método para compatibilidade com a biblioteca react-native-screens.
-   * A chamada `super.onCreate(null)` é intencional para corrigir um problema de restauração de estado.
-   */
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(null)
   }
