570a1b3af7f3992d40a833bf757ad16396ec4f6b
patamarcelo <patamarcelo@gmail.com>
2025-07-05 19:49:29 -0300

    updated flobal btoa func

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 import { registerRootComponent } from 'expo';
+import { decode, encode } from 'base-64'
+
+if (!global.btoa) {
+    global.btoa = encode;
+}
+
+if (!global.atob) {
+    global.atob = decode;
+}
+
 
 import App from './App';
 
